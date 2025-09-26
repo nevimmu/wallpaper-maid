aeefc20c58d3cd1738dd35e63cf47ce2b2abbfa1
Nevimmu <nevimmu@gmail.com>
2025-09-27 00:32:53 +0200

    Bump version: 0.0.1 → 0.1.0

--- src/wallpaper_maid/settings.py
+++ src/wallpaper_maid/settings.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 HOME = os.getenv('HOME', os.getenv('USERPROFILE'))
 XDG_CONF_DIR = os.getenv('XDG_CONFIG_HOME', os.path.join(HOME, '.config'))
